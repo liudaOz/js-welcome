6f78f08377c3f098f743fa359434baee79acce00
“liudaOz” <liuda.oz2023js@gmail.com>
2023-12-19 19:15:37 +0200

    data types

--- index.js
+++ index.js
@@ -1,9 +1,27 @@
 //  дужки () - це оператор виклику функції, в дужках - це аргументи
 
-console.log(55);
+//console.log(55);
 
-alert("Hello world !");
+//alert('Hello world !');
 
 // Інтерфейс - це місце взаємодії між користувачем і системою, як касир банку
 // в HTML використовуємо подвійні лапки " "
 // в JavaScript використовуємо одинарні лапки ' '
+
+//console.log('test');
+
+/*
+Інтерфейс - це місце взаємодії між користувачем і системою, як касир банку
+в HTML використовуємо подвійні лапки ""
+в JavaScript використовуємо одинарні лапки ''
+Кожний тип даних по своєму виділяється в console.log()
+
+*/
+
+console.log(false);
+console.log(true);
+console.log('string');
+console.log(50);
+console.log(null);
+console.log(undefined);
+console.log(5n);  //bigint
